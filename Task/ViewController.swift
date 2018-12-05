6e970cc6c6016452bf6c2ecd44c531a5a7e2eb9e
Steve Lederer <stevieled@gmail.com>
2018-12-04 22:06:29 -0700

    Initial Commit

---
+++ Task/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Task
+//
+//  Created by Steve Lederer on 12/4/18.
+//  Copyright © 2018 Steve Lederer. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
